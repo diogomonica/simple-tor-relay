9ae69cff16fa3595796df0c798e7513aead92f0d
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2014-11-09 21:26:01 -0500

    Back-end fixes, port forwarding works, torrc is being generated correctly and fixed a Tor+Ubuntu 14.04 apparmor bug

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ apt-get -y upgrade
 
 ### INSTALL THE TOR CLIENT
 # https://www.torproject.org/docs/debian.html.en#ubuntu
-echo "deb http://deb.torproject.org/torproject.org `lbs_release -cs` main" >> /etc/apt/sources.list
+echo "deb http://deb.torproject.org/torproject.org `lsb_release -cs` main" >> /etc/apt/sources.list
 
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
@@ -47,29 +47,70 @@ EOF
 # https://www.torproject.org/docs/tor-relay-debian.html.en#setup
 apt-get -y install openntpd
 
-exit 0
+# Set-up port forwards.
+echo 1 > /proc/sys/net/ipv4/conf/eth0/forwarding 
+cat > /etc/iptables.rules <<EOF
+*nat
+:PREROUTING ACCEPT [368:102354]
+:POSTROUTING ACCEPT [0:0]
+:OUTPUT ACCEPT [92952:20764374]
+-A PREROUTING -p tcp -i eth0 -j REDIRECT -m tcp --dport 443 --to-ports 9001
+-A PREROUTING -p tcp -i eth0 -j REDIRECT -m tcp --dport 80 --to-ports 9030
+COMMIT
+EOF
 
-# TODO: Actually do DirPort?
-# Relay
-echo >> /etc/tor/torrc <<EOF
+cat > /etc/network/if-pre-up.d/iptables <<EOF
+#!/bin/sh
+/sbin/iptables-restore < /etc/iptables.rules
+EOF
+chmod +x /etc/network/if-pre-up.d/iptables
+
+apt-get -y install ruby
+
+# Patch https://trac.torproject.org/projects/tor/ticket/13716
+echo 'signal (send) set=("term") peer="unconfined",' >>/etc/apparmor.d/local/system_tor
+apparmor_parser -r /etc/apparmor.d/system_tor
+
+# Use ERb templating to generate the torrc
+# TODO: Actually do DirPort? Is it covered under Accounting?
+ruby -rerb -rostruct -e 'puts ERB.new($stdin.read, nil, "-").result(OpenStruct.new(:config => ARGV[0], :bandwidth_cap => ARGV[1]).instance_eval { binding })' private_bridge 51200000 > /etc/tor/torrc <<EOF
 # Don't open a local SOCKS port, this host is just running a relay/bridge
 SocksPort 0
+
+# This is not an exit node
+ExitPolicy reject *:*
 
 # Advertise our directory/onion ports as 80/443 but actually listen on high ports and redirect
 #  requests using iptables rules (to avoid problems with the Tor Daemon binding on low ports.)
 ORPort 443 NoListen
-ORPort 127.0.0.1:9001 NoAdvertise
-DirPort 80 NoListen
-DirPort 127.0.0.1:9030 NoAdvertise
+ORPort 0.0.0.0:9001 NoAdvertise
+#DirPort 80 NoListen
+#DirPort 0.0.0.0:9030 NoAdvertise
 
-# Never send or receive more than 10GB of data per week. The accounting
-# period runs from 10 AM on the 1st day of the week (Monday) to the same
-# day and time of the next week.
-AccountingStart week 1 10:00
-AccountingMax 128 GB
-BandwidthRate 250KB
+# Cap our monthly bandwidth consumption so we don't go beyond 1/4 of our monthly allowance.
+AccountingStart month 1 00:00
+AccountingMax <%= bandwidth_cap.to_i / 2 %>b
+# Should we even have this here? Do we care?
+BandwidthRate 1MB
 BandwidthBurst 1GB
 
-# This is not an exit node
-ExitPolicy reject *:*
+# TODO Nickname
+
+<% if config == 'private_bridge' || config == 'bridge' -%>
+# Bridge configuration
+BridgeRelay 1
+ServerTransportPlugin obfs2,obfs3 exec /usr/bin/obfsproxy --managed
+ServerTransportListenAddr obfs2 0.0.0.0:<%= 1024 + rand(65535 - 1024) %>
+ServerTransportListenAddr obfs3 0.0.0.0:<%= 1024 + rand(65535 - 1024) %>
+<% end -%>
+
+<% if config == 'private_bridge' -%>
+# This bridge is private, don't advertise it.
+PublishServerDescriptor 0
+<% end -%>
+
+<% if config == 'relay' -%>
+<% end -%>
 EOF
+
+service tor start
